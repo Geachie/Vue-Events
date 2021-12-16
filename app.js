93315e0e9f06117aa8f82a88d166a2ecd2c1917a
Paul McGeachie <paulmcgeachie@icloud.com>
2021-12-16 21:07:22 +0000

    add native event object

--- app.js
+++ app.js
@@ -2,14 +2,18 @@ const app = Vue.createApp({
   data() {
     return {
       counter: 0,
+      name: "",
     };
   },
   methods: {
-    add() {
-      this.counter++;
+    setName(event, lastName) {
+      this.name = event.target.value + " " + lastName;
     },
-    remove() {
-      this.counter--;
+    add(num) {
+      this.counter = this.counter + num;
+    },
+    remove(num) {
+      this.counter = this.counter - num;
     },
   },
 });
